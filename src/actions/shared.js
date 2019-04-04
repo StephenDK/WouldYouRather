e1580c93ebeb7813f606c96e2778059e1633fab6
Stephen Klein <stephendklein08@gmail.com>
2019-04-03 19:17:23 -0700

    Shared.js initial data function

---
+++ src/actions/shared.js
@@ -0,0 +1,26 @@
+// get initial data from api
+import { getInitialData } from '../utils/api'
+
+// redux loading bar 
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+// action creators
+import { receiveUsers } from './users'
+import { receiveQuestions } from './questions'
+import { setAuthedUser } from './authedUser'
+
+
+
+//  export handle initial data function
+export function handelInitialData(userId) {
+    return (dispatch) => {
+        dispatch(showLoading())
+            return getInitialData()
+                .then(({ users, questions }) => {
+                    dispatch(setAuthedUser(userId ? userId : null))
+                    dispatch(receiveUsers(users))
+                    dispatch(receiveQuestions(questions))
+                    dispatch(hideLoading())
+                })
+    }
+}
