b62e7dba6a2127f0f473493eaa19c83f632c2e5a
StephenDK <stephendklein08@gmail.com>
2019-04-10 22:02:56 -0700

    Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,83 @@
 import React, { Component } from 'react'
-// connect component to store
 import { connect } from 'react-redux'
+import Question from './Question'
+
+import { createSelector } from 'reselect'
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <div className='toggle-catagory'>
-                    <select onChange={this.handleToggleCategory}>
-                        <option value='Unanswered Questions'>Unanswered Questions</option>
-                        <option value='Answered Questions'>Answered Questions</option>
-                    </select>
-                </div>
-            </div>
-        )
-    }
+  state = {
+    category: 'Unanswered Questions'
+  }
+
+  handleToggleCategory = e => {
+    const category = e.target.value
+
+    this.setState(() => ({
+      category
+    }))
+  }
+
+  render() {
+    const { category } = this.state
+    const { unansweredQuestions, answeredQuestions } = this.props
+    return (
+      <div>
+        <div className="category-toggler">
+          <select onChange={this.handleToggleCategory}>
+            <option value="Unanswered Questions">Unanswered Questions</option>
+            <option value="Answered Questions">Answered Questions</option>
+          </select>
+        </div>
+
+        {category === 'Unanswered Questions' && (
+          <ul className="questions">
+            {unansweredQuestions.map(id => (
+              <li key={id} className="question">
+                <Question id={id} />
+              </li>
+            ))}
+          </ul>
+        )}
+
+        {category === 'Answered Questions' && (
+          <ul className="questions">
+            {answeredQuestions.map(id => (
+              <li key={id} className="question">
+                <Question id={id} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
 }
+
+const fetchUnansweredQuestions = createSelector(
+  state => state.questions,
+  state => Object.keys(state.users[state.authedUser].answers),
+  state => Object.keys(state.questions),
+  (questions, answeredQuestions, questionsId) =>
+    questionsId
+      .filter(id => !answeredQuestions.includes(id))
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+)
+
+const fetchAnsweredQuestions = createSelector(
+  state => state.questions,
+  state => Object.keys(state.users[state.authedUser].answers),
+  (questions, answers) =>
+    answers.sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+)
 
 function mapStateToProps(state) {
-    const { users, questions } = state
-    return {
-        users,
-        questions,
-        answeredQuestions: fetchAnsweredQuestions(),
-        unansweredQuestions: fetchUnansweredQuestions()
-    }
+  const { users, questions } = state
+  return {
+    users,
+    questions,
+    answeredQuestions: fetchAnsweredQuestions(state),
+    unansweredQuestions: fetchUnansweredQuestions(state)
+  }
 }
-
 
 export default connect(mapStateToProps)(Dashboard)
